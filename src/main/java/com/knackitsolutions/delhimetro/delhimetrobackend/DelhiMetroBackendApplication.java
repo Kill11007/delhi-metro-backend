8afd780b93f23463058978e54d3c0f17f052a36c
Mayank Kumar <mj112295@gmail.com>
2022-12-01 12:51:22 +0530

    First COmmit

---
+++ src/main/java/com/knackitsolutions/delhimetro/delhimetrobackend/DelhiMetroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.knackitsolutions.delhimetro.delhimetrobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelhiMetroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelhiMetroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knackitsolutions/delhimetro/delhimetrobackend/DelhiMetroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knackitsolutions.delhimetro.delhimetrobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelhiMetroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
